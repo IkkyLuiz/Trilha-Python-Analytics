6957955cf00c93dfb214ff5338ed675d7a4be9cf
Luiz Henrique <81529238+IkkyLuiz@users.noreply.github.com>
2024-04-21 11:01:59 -0300

    criando a pasta fundamentos python e adicionando exercícios de fixação.

---
+++ FudamentosPython/conversacao_Python.py
@@ -0,0 +1,12 @@
+print(int(2.97348728))
+print(int("10"))
+print(float("10.10"))
+print(float(100))
+
+valor = 5
+valor_str = str(valor)
+print(type(valor))
+print(type(valor_str))
+
+print(100 / 5)
+print(100 // 5)
